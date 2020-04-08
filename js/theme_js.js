2e21507f9dd86ea5f7526ffc688e2f37301fa283
Magno <62749818+demedeirosmagno@users.noreply.github.com>
2020-04-08 07:17:51 -0300

    Create theme_js.js

---
+++ js/theme_js.js
@@ -0,0 +1,12 @@
+$(document).animateScroll();  
+		//***menu-mobile***//
+		$(".button-collapse").sideNav();
+		//***carrousel-fullwidth***//
+		$('.carousel.carousel-slider').carousel({
+		    'fullWidth' : false,
+		    'duration' : 5,
+		    'shift' : 10,
+		    'numVisible' : 0,
+		    'indicators' : true,
+		    
+		  });
